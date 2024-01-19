6ea7018ee66a5fcd6bae452787f90f38e7869131
André van der Goes <andre.van.der.goes@macaw.nl>
2024-01-19 10:52:33 +0100

    bugfixes

--- PsScripts/MessageBird/README.md
+++ PsScripts/MessageBird/README.md
@@ -8,7 +8,7 @@ The script accepts the following parameters:
 A string containing the message you want to be send through SMS  
 Example: "This is a message"
 
-**[Required] _recipient_**  
+**[Optional] _recipient_**  
 An array of strings containing the (internation) mobile numbers you want to send the SMS to  
 Example 1: "+31612312345"  
 Example 2: "+31612312345", "+31612312354"  
@@ -27,6 +27,7 @@ The configuration file should look like this:
 {
   "apiKey": "",
   "originator": "+31231231123",
+  "recipient": [],
   "secureApiKey": "",
   "statusTimeoutInMins": 5,
   "writeLog": true,
@@ -36,6 +37,7 @@ The configuration file should look like this:
 ```
 - **_apiKey_**: Cleartext string containing the apiKey (see below)
 - **_originator_**: The phonenumber that is configured as originator within the MessageBird SMS Channel
+- **_recipient_**: The array of phonenumbers that will receive the message. Only used if not set as parameter when calling the script
 - **_secureApiKey_**: This value will be written by the script, do not fill manually (see below)
 - **_statusTimeoutInMins_**: The amount of time the script waits until all SMS delivery is successfull. If not provided it defaults to 5 minutes
 - **_writeLog_**: Boolean. If true the script will log to file
@@ -43,6 +45,7 @@ The configuration file should look like this:
 - **_logFileBaseName_**: The base name for the logfile. The filename will be updated with the current date. For instance if "messagebird.log" is provided, the logfile could be named: messagebird-20240117.log
 
 ### apiKey and secureApiKey
+**_If the script will be running under a different account than the user copying the files, make sure that this account has permissions to update the config file(!)_**
 When preparing the script for first use, you have to set the apiKey to authenticate to MessageBird as a cleartext string in the configuration file. When the script is run the first time, it will read the apiKey. If the apiKey is valid and authentication to MessageBird succeeds, the script will encrypt it and write it out to the config file as secureApiKey. The cleartext apiKey will be removed at this point.
 **_Please Note: The secureApiKey is only readable for the user that the script was run under the first time and ONLY on the machine the string was encrypted on!_**
 If the apiKey needs to be updated, you can just set a new value in the configuration file and run the script again (using the correct user context) .
--- PsScripts/MessageBird/send-messagebirdmsg.ps1
+++ PsScripts/MessageBird/send-messagebirdmsg.ps1
@@ -1,69 +1,69 @@
 Param (
     [Parameter(Mandatory=$true)]
-    [string]$message,
-
-    [Parameter(Mandatory=$true)]
-    [string[]]$recipient,
+    [string]$Message,
 
     [Parameter(Mandatory=$false)]
-    [string]$configFileFullname = (Join-Path -Path $PSScriptRoot -ChildPath "messagebird.config.json")
+    [string[]]$Recipient,
+
+    [Parameter(Mandatory=$false)]
+    [string]$ConfigFilePath = (Join-Path -Path $PSScriptRoot -ChildPath "messagebird.config.json")
 )
 
 
 Function Write-Log {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$message,
+        [string]$Message,
 
         [Parameter(Mandatory=$false)]
-        [string]$logFileFullname,
+        [string]$LogFilePath,
 
         [Parameter(Mandatory=$false)]
-        [switch]$warning,
+        [switch]$Warning,
 
         [Parameter(Mandatory=$false)]
-        [switch]$logOnly
+        [switch]$LogOnly
     )
     $now = Get-Date -UFormat "%m-%d-%Y %R %Z"
     $outMessage = ("{0} {1}" -f $now, $message)
 
-    if (-Not $logOnly) {
-        if ($warning) {
+    if (-Not $LogOnly) {
+        if ($Warning) {
             Write-Warning $outMessage
-            $outMessage = ("{0} WARNING {1}" -f $message)
+            $outMessage = ("{0} WARNING {1}" -f $now, $message)
         } else {
             Write-Host $outMessage
         }
     }
 
-    if ($logFileFullname) {
-        $outMessage | Out-File -FilePath $logFileFullname -Append
+    if ($LogFilePath) {
+        $outMessage | Out-File -FilePath $LogFilePath -Append
     }
 }
 
 Function Get-LogFile {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$logFileBaseName,
+        [string]$LogFileBaseName,
 
         [Parameter(Mandatory=$false)]
-        [string]$logFileRelativePath
+        [string]$LogFileRelativePath
     )
-    $logFileName = $logFileBaseName.Substring(0, $logFileBaseName.lastIndexOf('.'))
-    $logExtension = $logFileBaseName.Split('.')[-1]
+    $logFileName = $LogFileBaseName.Substring(0, $LogFileBaseName.lastIndexOf('.'))
+    $logExtension = $LogFileBaseName.Split('.')[-1]
 
-    if ($logFileRelativePath) {
-        $logFilePath = Join-Path -Path $PSScriptRoot -ChildPath $logFileRelativePath
+    if ($LogFileRelativePath) {
+        $logFilePath = Join-Path -Path $PSScriptRoot -ChildPath $LogFileRelativePath
         [void](New-Item -Path $logFilePath -ItemType Directory -Force)
     } else {
         $logFilePath = $PSScriptRoot
     }
 
-    $logFileFullname = Join-Path -Path $logFilePath -ChildPath ("{0}-{1}.{2}" -f $logFileName, (Get-Date -Format FileDate), $logExtension)
-    if (-Not (Test-Path -Path $logFileFullname)){
-        $log = New-Item -Path $logFileFullname -ItemType File
+    $logFilePath = Join-Path -Path $logFilePath -ChildPath ("{0}-{1}.{2}" -f $logFileName, (Get-Date -Format FileDate), $logExtension)
+    if (-Not (Test-Path -Path $LogFilePath)){
+        $log = New-Item -Path $LogFilePath -ItemType File
     } else {
-        $log = Get-Item -Path $logFileFullname
+        $log = Get-Item -Path $logFilePath
     }
     return $log.FullName
 }
@@ -71,15 +71,15 @@ Function Get-LogFile {
 Function Get-ApiKey {
     param (
         [Parameter(Mandatory=$true)]
-        [object]$config
+        [object]$Config
     )
     # check if secureApiKey is set
-    if ([string]::IsNullOrEmpty($config.secureApiKey) -and [string]::IsNullOrEmpty($config.apiKey)){
-        Write-Log -message "Config file does not contain an API key" -logFileFullname $logFile -warning -logOnly
+    if ([string]::IsNullOrEmpty($Config.secureApiKey) -and [string]::IsNullOrEmpty($Config.apiKey)){
+        Write-Log -Message "Config file does not contain an API key" -LogFilePath $logFile -Warning -LogOnly
         throw "Config file does not contain an API key"
     # presuming an apikey is at least 5 characters
-    } elseif ($config.apiKey.Length -gt 5) {
-        Write-Log -message "Writing new secure API key to config file" -logFileFullname $logFile
+    } elseif ($Config.apiKey.Length -gt 5) {
+        Write-Log -message "Writing new secure API key to config file" -LogFilePath $logFile
         # check the key to make sure it is valid
         $headers = @{
             Authorization = ("AccessKey {0}" -f $config.apiKey)
@@ -88,67 +88,67 @@ Function Get-ApiKey {
         Try {
             [Void](Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ErrorAction Stop)
         } Catch {
-            Write-Log -message ("API key is not valid: {0}" -f $_.Exception.Message) -logFileFullname $logFile -warning -logOnly
+            Write-Log -Message ("API key is not valid: {0}" -f $_.Exception.Message) -LogFilePath $logFile -Warning -LogOnly
             throw "API key is not valid"
         }
         # if the config does not contain a member secureApiKey, add it
-        if (-Not ($config | Get-Member -MemberType NoteProperty -Name secureApiKey)){
-        $config | 
+        if (-Not ($Config | Get-Member -MemberType NoteProperty -Name secureApiKey)){
+        $Config | 
             Add-Member `
                 -MemberType NoteProperty `
                 -Name secureApiKey `
                 -Value (ConvertTo-SecureString -String $config.apiKey -AsPlainText -Force | ConvertFrom-SecureString)
         } else {
             # if the config does contain a member secureApiKey, update it
-            $config.secureApiKey = ConvertTo-SecureString -String $config.apiKey -AsPlainText -Force | ConvertFrom-SecureString
+            $Config.secureApiKey = ConvertTo-SecureString -String $Config.apiKey -AsPlainText -Force | ConvertFrom-SecureString
         }
-        $config.apiKey = ""
+        $Config.apiKey = ""
     }
-    return $config
+    return $Config
 }
 
 Function Update-Config {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$configFileFullname,
+        [string]$ConfigFilePath,
 
         [Parameter(Mandatory=$true)]
-        [object]$config
+        [object]$Config
     )
    
-    if (-Not $config.secureApiKey -or -Not [string]::IsNullOrEmpty($config.apiKey)) {
+    if (-Not $Config.secureApiKey -or -Not [string]::IsNullOrEmpty($Config.apiKey)) {
         # Either the config does not contain a secureApiKey or apiKey is not empty
-        $config = Get-ApiKey -config $config
+        $Config = Get-ApiKey -config $Config
         # Write out the config file (Note: the secureApiKey is only reable by the user that created it on the machine it was created on)
-        $config | ConvertTo-Json | Set-Content -Path $configFileFullname
+        $Config | ConvertTo-Json | Set-Content -Path $ConfigFilePath
     }
-    return $config
+    return $Config
 }
 
 Function Send-MessageBirdMsg {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$message,
+        [string]$Message,
         [Parameter(Mandatory=$true)]
-        [string[]]$recipient,
+        [string[]]$Recipient,
         [Parameter(Mandatory=$true)]
-        [string]$originator,
+        [string]$Originator,
         [Parameter(Mandatory=$true)]
-        [string]$apiKey
+        [string]$ApiKey
     )
     $body = @{
-        recipients = $recipient -join ','
-        originator = $originator
-        body = $message
+        recipients = $Recipient -join ','
+        originator = $Originator
+        body = $Message
     }
     $headers = @{
-        Authorization = "AccessKey $apiKey"
+        Authorization = "AccessKey $ApiKey"
     }
     $uri = "https://rest.messagebird.com/messages"
     try {
         $response = Invoke-RestMethod -Method Post -Uri $uri -Body $body -Headers $headers
     } Catch {
-        Write-Log -message ("Error sending message: {0}" -f $_.Exception.Message) -logFileFullname $logFile -warning -logOnly
+        Write-Log -Message ("Error sending message: {0}" -f $_.Exception.Message) -LogFilePath $logFile -Warning -LogOnly
         throw $_
     }
     return $response
@@ -157,24 +157,24 @@ Function Send-MessageBirdMsg {
 Function Get-MessageBirdDeliveryStatus {
     param(
         [Parameter(Mandatory=$true)]
-        [string]$messageId,
+        [string]$MessageId,
         [Parameter(Mandatory=$true)]
-        [string]$apiKey,
+        [string]$ApiKey,
         [Parameter(Mandatory=$true)]
-        [int]$statusTimeoutInMins
+        [int]$StatusTimeoutInMins
     )
     $now = Get-Date
     $headers = @{
-        Authorization = "AccessKey $apiKey"
+        Authorization = "AccessKey $ApiKey"
     }
-    $uri = ("https://rest.messagebird.com/messages/{0}" -f $messageId)
-    while ((Get-Date) -ne $now.AddMinutes($statusTimeoutInMins)) {
+    $uri = ("https://rest.messagebird.com/messages/{0}" -f $MessageId)
+    while ((Get-Date) -lt $now.AddMinutes($StatusTimeoutInMins)) {
         Try {
             $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ErrorAction Stop
         } Catch {
             # it can take a moment for the message to be available through the API
             if (-Not ($_.Exception.Response.StatusCode -eq "NotFound")) {
-                Write-Log -message ("Error getting message status: {0}" -f $_.Exception.Message) -logFileFullname $logFile -warning -logOnly
+                Write-Log -Message ("Error getting message status: {0}" -f $_.Exception.Message) -LogFilePath $logFile -Warning -LogOnly
                 throw $_
             }
         }
@@ -187,38 +187,44 @@ Function Get-MessageBirdDeliveryStatus {
 }
 
 # Get config
-if (-Not (Test-Path -Path $configFileFullname)){
-    throw "Config file not found: $configFileFullname"
+if (-Not (Test-Path -Path $ConfigFilePath)){
+    throw "Config file not found: $ConfigFilePath"
 }
-$config = Get-Content -Path $configFileFullname -Raw | ConvertFrom-Json
+$config = Get-Content -Path $ConfigFilePath -Raw | ConvertFrom-Json
 
 # start logging
 if ($config.writeLog) {
-    $logFile = Get-LogFile -logFileRelativePath $config.logFileRelativePath -logFileBaseName $config.logFileBaseName
+    $logFile = Get-LogFile -LogFileRelativePath $config.logFileRelativePath -LogFileBaseName $config.logFileBaseName
 } else {
     $logFile = ""
 }
 
-$config = Update-Config -configFileFullname $configFileFullname -config $config
+$config = Update-Config -ConfigFilePath $ConfigFilePath -Config $config
 # Extract and decrypt the secureApiKey
-$apiKey = $config.secureApiKey | ConvertTo-SecureString | ConvertFrom-SecureString -AsPlainText
+$apiKey = $config.secureApiKey | ConvertTo-SecureString -ErrorAction Stop | ConvertFrom-SecureString -AsPlainText
 $statusTimeoutInMins = $config.statusTimeoutInMins ? $config.statusTimeoutInMins : 5
 
+$recipient = $recipient ? $recipient : $config.recipient
+if (-Not $recipient) {
+    Write-Log -message "No recipient specified" -LogFilePath $logFile -Warning -LogOnly
+    throw "No recipient specified"
+}
+
 # send the message
-Write-Log -message ("Sending message '{0}'" -f $message) -logFileFullname $logFile
-Write-Log -message ("Sending message to {0} with originator {1}" -f ($recipient -join ','), $config.originator) -logFileFullname $logFile
-$response = Send-MessageBirdMsg -message $message -recipient $recipient -originator $config.originator -apiKey $apiKey
+Write-Log -Message ("Sending message '{0}'" -f $message) -LogFilePath $logFile
+Write-Log -Message ("Sending message to {0} with originator {1}" -f ($recipient -join ','), $config.originator) -LogFilePath $logFile
+$response = Send-MessageBirdMsg -Message $message -Recipient $recipient -Originator $config.originator -ApiKey $apiKey
 
 # get the delivery status
-Write-Log -message  ("Checking status for message with id {0}" -f $response.id) -logFileFullname $logFile
-$status = Get-MessageBirdDeliveryStatus -messageId $response.id -apiKey $apiKey
+Write-Log -Message  ("Checking status for message with id {0}" -f $response.id) -LogFilePath $logFile
+$status = Get-MessageBirdDeliveryStatus -MessageId $response.id -ApiKey $apiKey -StatusTimeoutInMins $statusTimeoutInMins
 
 if ($response.recipients.totalDeliveryFailedCount -gt 0) {
-    Write-Log -message "Message delivery failed" -logFileFullname $logFile -warning
+    Write-Log -Message "Message delivery failed" -LogFilePath $logFile -Warning
     $status.recipients.items | Where-Object {-Not [int]$_.statusErrorCode -eq 0}  | ForEach-Object {
-        Write-Log -message ("message failed to deliver to recipient {0}, status {1}({2})" -f $_.recipient, $_.statusReason, $_.statusErrorCode) -logFileFullname $logFile -warning
+        Write-Log -Message ("message failed to deliver to recipient {0}, status {1}({2})" -f $_.recipient, $_.statusReason, $_.statusErrorCode) -LogFilePath $logFile -Warning
     }
 }
 $status.recipients.items | Where-Object {[int]$_.statusErrorCode -eq 0}  | ForEach-Object {
-    Write-Log -message ("Message delivered to recipient {0}, status {1})" -f $_.recipient, $_.statusReason) -logFileFullname $logFile
+    Write-Log -Message ("Message delivered to recipient {0}, status {1})" -f $_.recipient, $_.statusReason) -LogFilePath $logFile
 }
